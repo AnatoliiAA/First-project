c107170981ec42f76ee341089b139a6f733541b0
AnatoliiAA <cincun11@gmail.com>
2018-10-29 22:51:01 +0300

    Valid html, not final version

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel();
+});
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    items:3,
+    merge:true,
+    autoplay:true
+    
+})
